52d793339adff052b9f8be551b71c541303e1962
Dark-Raine <anderson-tomas@hotmail.com>
2019-01-29 15:16:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {};
+
+let updateDriverWithKeyAndValue = (driver, key, value) => {
+    let modDriver = {...driver};
+    modDriver[key] = value;
+    return modDriver;
+}
+
+let destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+    driver[key] = value;
+    return driver;
+}
+
+let deleteFromDriverByKey = (driver, key) => {
+    let modDriver = {...driver};
+    delete modDriver[key];
+    return modDriver;
+}
+
+let destructivelyDeleteFromDriverByKey = (driver, key) => {
+    delete driver[key];
+    return driver;
+}
